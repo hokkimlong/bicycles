85f700043baeaf57d03bd92af9a7624c35ebc32f
long <hokkimlong@outlook.com>
2020-09-21 17:15:38 +0700

    deploy to heroku

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 #\app.py
+import os
 from bottle import route,run
 
 @route('/')
@@ -13,4 +14,7 @@ def main():
 def main():
     return "apple"
 
-run(host='localhost',port=3000,debug=True,reloader=True)
+if 'DYNO' in os.environ:
+  run(host='0.0.0.0', port=os.environ.get('PORT', 9000))
+else: 
+  run(host='localhost', port=9000, debug=True, reloader=True)
